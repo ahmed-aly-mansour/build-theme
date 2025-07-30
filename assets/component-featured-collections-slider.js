f5ce01e11629d51a102f67a779bc07808bebd1a3
ahmed-aly-mansour <a.mansour.dev1@gmail.com>
2025-07-30 23:03:07 +0300

    fix collection slider

---
+++ assets/component-featured-collections-slider.js
@@ -0,0 +1,72 @@
+class FeaturedCollections extends HTMLElement {
+  constructor() {
+    super();
+    this.swiper = null;
+    this.sectionId = this.getAttribute('data-section-id');
+
+    // Store bound methods so add/removeEventListener match
+    this.onSectionLoad = this.onSectionLoad.bind(this);
+    this.onSectionUnload = this.onSectionUnload.bind(this);
+  }
+
+  connectedCallback() {
+    this.init();
+    document.addEventListener('shopify:section:load', this.onSectionLoad);
+    document.addEventListener('shopify:section:unload', this.onSectionUnload);
+  }
+
+  disconnectedCallback() {
+    this.destroy();
+    document.removeEventListener('shopify:section:load', this.onSectionLoad);
+    document.removeEventListener('shopify:section:unload', this.onSectionUnload);
+  }
+
+  init() {
+    if (window.Swiper && this.sectionId) {
+      const selector = `#featured-collections-${this.sectionId}`;
+      const swiperEl = document.querySelector(selector);
+
+      // Prevent duplicate swiper initialization
+      if (!swiperEl || swiperEl.classList.contains('swiper-initialized')) return;
+
+      this.swiper = new Swiper(selector, {
+        slidesPerView: 1,
+        autoHeight: false,
+        breakpoints: {
+          450: {
+            slidesPerView: 2,
+          },
+          700: {
+            slidesPerView: 4,
+          }
+        },
+        loop: true,
+        watchOverflow: false,
+      });
+    }
+  }
+
+  destroy() {
+    if (this.swiper) {
+      this.swiper.destroy(true, true);
+      this.swiper = null;
+    }
+  }
+
+  onSectionLoad(e) {
+    if (e.detail && e.detail.sectionId === this.sectionId) {
+      this.destroy();
+      this.init();
+    }
+  }
+
+  onSectionUnload(e) {
+    if (e.detail && e.detail.sectionId === this.sectionId) {
+      this.destroy();
+    }
+  }
+}
+
+if (!customElements.get('featured-collections')) {
+  customElements.define('featured-collections', FeaturedCollections);
+}
